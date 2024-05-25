d030b491870a4d3befa9d51c02d6810220ac641f
Michael Copland <mjbcopland@gmail.com>
2024-05-25 07:22:19 +0100

    Vite

---
+++ web/src/main.tsx
@@ -0,0 +1,11 @@
+import { createElement } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement!);
+
+const app = createElement(App);
+
+root.render(app);
