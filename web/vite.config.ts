03417bbddff362510d1a829059c0551d8e12f7f9
Michael Copland <mjbcopland@gmail.com>
2024-05-26 22:00:14 +0100

    fix proxies

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,6 +3,14 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: { proxy: { "/api": { target: "http://localhost:8080/xi", rewrite: (path) => path.replace(/^\/api/, "") } } },
   plugins: [react()],
+
+  server: {
+    proxy: {
+      "/api": {
+        target: process.env.PROXY_TARGET || "http://localhost:8080",
+        rewrite: (path) => path.replace(/^\/api\//, "/xi/"),
+      },
+    },
+  },
 });
